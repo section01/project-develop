79f04043ac4a39f5ea2779dbb41bc63ef6f8c249
section01 <57982582+section01@users.noreply.github.com>
2022-03-16 20:57:19 +0900

    Add files via upload

---
+++ UserMapper.java
@@ -0,0 +1,41 @@
+package jp.co.web.infrastructure.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import jp.co.web.infrastructure.model.UserModel;
+
+/**
+ * ユーザリポジトリ
+ */
+@Mapper
+public interface UserMapper {
+
+	/**
+	 * 取得
+	 */
+    public UserModel findUserInformationByAuth(
+            @Param("account")  String account,
+            @Param("password") String password);
+
+    /**
+	 * 取得
+	 */
+    public UserModel findUserInformationByExist(
+            @Param("id")   String id,
+            @Param("name") String name);
+
+    /**
+     * 登録
+     */
+    public void insertUserInfo(
+            @Param("id")   String id,
+            @Param("name") String name);
+
+    /**
+     * 削除
+     */
+    public void deleteUserInfo(
+            @Param("id") String id);
+
+}
